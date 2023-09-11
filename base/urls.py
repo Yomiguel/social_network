c62ba60a7405320dce0d9b7e0f4970cd5dc671bb
Yomiguel <desarrollador8@procesamiento.com.co>
2023-09-11 07:50:38 -0500

    Form added n ordering rooms

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="Home"),
     path('room/<str:pk>/', views.room, name="Room"),
-    path('create-room', views.createRoom, name="Create-Room")
+    path('create-room/', views.createRoom, name="Create-Room"),
+    path('update-room/<str:pk>/', views.updateRoom, name="Update-Room"),
+    path('delete/<str:pk>/', views.deleteRoom, name="Delete"),
 ]
